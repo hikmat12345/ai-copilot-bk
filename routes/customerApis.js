690e9fe78af6194d5c89fff12ecf90eba8991417
Hikmat Ullah Khan <98313065+hikmat12345@users.noreply.github.com>
2023-05-13 22:26:22 +0500

    Update customerApis.js

--- routes/customerApis.js
+++ routes/customerApis.js
@@ -3,22 +3,25 @@ const router = express.Router()
 const Registeration = require('../models/Registeration')
 const ContactForm = require('../models/ContactForm');
  
-// create user signup
+// create user signup 
 router.post('/signup', async (req, res) => {
   const register = new Registeration({
+    name:req.body.name,
     mobileNumber: req.body.mobileNumber,
     password: req.body.password,
     email: req.body.email,
   })
   try {
     const loggedIn = await register.save()
-    res
-      .status(201)
-      .json({ message: 'successfuly created', status: true, error: false })
+    if (res) {
+      await Registeration.findOne({ email: req.body.email }, function (err, doc) {
+        return res.status(200).send({ message:'successfuly created', userDetail: doc, optional:err, statu: true })
+      })
+    } 
   } catch (err) {
-    res.status(400).json({ message: err.message })
+    res.status(403).json({ message: err.message })
   }
-}) 
+})
 
 // Admin Login
 router.post('/signin', (req, res) => {
