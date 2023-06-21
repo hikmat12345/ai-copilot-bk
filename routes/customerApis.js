af2ccec6ba6aa4ff7ec19409c6a27f15a2d40e1d
Hikmat Ullah Khan <98313065+hikmat12345@users.noreply.github.com>
2023-06-21 17:22:24 +0500

    Update customerApis.js

--- routes/customerApis.js
+++ routes/customerApis.js
@@ -286,5 +286,56 @@ router.post('/create-checkout-session', async (req, res) => {
 
   res.json({ id: session.id });
 });
- 
+
+
+
+
+
+
+async createSubscription(createSubscriptionRequest) {
+   const { name, address, email ,city, postcode, userid, amount, id, planId, duration} = req.body;
+
+    // create a stripe customer
+    const customer = await this.stripe.customers.create({
+      name: createSubscriptionRequest.name,
+      email: createSubscriptionRequest.email,
+      payment_method: createSubscriptionRequest.id,
+      invoice_settings: {
+        default_payment_method: createSubscriptionRequest.id,
+      },
+    });
+
+
+    // get the price id from the front-end
+    const priceId = "price_1NLNoSJzkqBjcDruuy8ZBOFW";
+
+    // create a stripe subscription
+    const subscription = await this.stripe.subscriptions.create({
+      customer: customer.id,
+      items: [{ price: priceId }],
+      payment_settings: {
+        payment_method_options: {
+          card: {
+            request_three_d_secure: 'any',
+          },
+        },
+        payment_method_types: ['card'],
+        save_default_payment_method: 'on_subscription',
+      },
+      expand: ['latest_invoice.payment_intent'],
+    });
+
+    // return the client secret and subscription id
+    return {
+      clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+      subscriptionId: subscription.id,
+    };
+  }
+
+
+router.post('/create-subscription', ( req  ,res ) => {
+
+    createSubscription(req);
+
+})
 module.exports = router
