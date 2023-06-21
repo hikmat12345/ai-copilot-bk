0237ab384f5a4491f5397bfa3295c73a005c4cdb
Hikmat Ullah Khan <98313065+hikmat12345@users.noreply.github.com>
2023-06-21 16:49:23 +0500

    Update customerApis.js

--- routes/customerApis.js
+++ routes/customerApis.js
@@ -271,7 +271,7 @@ router.post("/create-payment-intent", async (req, res) => {
 
 
 
-route.post('/create-checkout-session', async (req, res) => {
+router.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     line_items: [
